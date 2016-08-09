5e3115258208679968ba9ee06b708172fe503045
oshai <ohadshai@gmail.com>
2016-08-09 23:46:49 +0300

    added android.util.Log logging

--- app/src/main/java/mu/kotlinloggingexampleandroid/MainActivity.kt
+++ app/src/main/java/mu/kotlinloggingexampleandroid/MainActivity.kt
@@ -2,9 +2,11 @@ package mu.kotlinloggingexampleandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
