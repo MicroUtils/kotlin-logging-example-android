f2aaa481868c4e19ad250d2a80fe120f283981cf
oshai <ohadshai@gmail.com>
2016-08-09 23:58:19 +0300

    added kotlin-logging

--- app/src/main/java/mu/kotlinloggingexampleandroid/MainActivity.kt
+++ app/src/main/java/mu/kotlinloggingexampleandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package mu.kotlinloggingexampleandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import mu.KLogging
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v(TAG, "android.util.Log logging");
+        Log.v(TAG, "This is logging of - android.util.Log logging");
+        logger.info { "This is logging of - kotlin-logging" }
     }
+
+    companion object: KLogging()
 }
